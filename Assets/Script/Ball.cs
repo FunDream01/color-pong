7fdf6189a14f188e4584e9325106825bc7de4d10
Abdulrahman Adham <46246621+FunDream01@users.noreply.github.com>
2020-03-26 23:22:08 +0300

    Hello World

---
+++ Assets/Script/Ball.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float BallSpeed = 5f;
+    [HideInInspector]
+    public bool moveAss = false;
+    [HideInInspector]
+    public bool canClone = false;
+    public float DelayTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("startCanCloning", DelayTime);
+    
+    }
+
+    public void moveAllAsses()
+    {
+        
+        float sx = Random.Range(0, 2) == 0 ? -1 : 1;
+        float sy = Random.Range(0, 2) == 0 ? -1 : 1;
+
+        GetComponent<Rigidbody>().velocity = new Vector3(BallSpeed*sx, BallSpeed * sy , 0f);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)&& moveAss == false)
+        {
+            moveAllAsses();
+            moveAss = true;
+        }
+
+        
+    }
+
+    void startCanCloning() 
+    {
+        canClone = true;
+    }
+}
---
+++ Assets/Script/PlayerContoller.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector2 LastTapPos;
+    public GameObject Player;
+    public float Speed;
+    public Rigidbody clonedAss;
+    void Start()
+    {
+        Player = this.gameObject; 
+
+        
+    }
+
+    // Update is called once per frame
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("myBallz"))
+        {
+           
+            //clone.velocity = transform.TransformDirection(Vector3.forward * 10);
+            if (collision.gameObject.GetComponent<Ball>().canClone == true)
+            {
+                Rigidbody clone;
+                clone = Instantiate(clonedAss, collision.transform.position, collision.transform.rotation);
+                clone.GetComponent<Ball>().moveAllAsses();
+                clone.GetComponent<Ball>().moveAss = true;
+            }
+        }
+
+    }
+
+
+
+    void Update()
+    {
+        //Mouse Slider Controller :
+        if (Input.GetMouseButton(0))
+        {
+
+            Vector2 curTapPos = Input.mousePosition;
+            if (LastTapPos == Vector2.zero)
+            {
+                LastTapPos = curTapPos;
+            }
+            float delta = LastTapPos.x - curTapPos.x;
+            LastTapPos = curTapPos;
+            Player.transform.Translate(new Vector3((-delta * Speed),0 , 0));
+            Vector3 pos = transform.position;
+            pos.x = Mathf.Clamp(transform.position.x, -8, 8);
+            transform.position = pos;
+        }
+        
+        
+        
+        
+        
+        if (Input.GetMouseButtonUp(0))
+        {
+            LastTapPos = Vector3.zero;
+
+        }
+
+
+        
+    } 
+}
